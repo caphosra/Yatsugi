cb628d82f68eb30fc9dd58863d9cec3021264dd2
Akihisa Yagi <capra314cabra@gmail.com>
2020-04-01 18:25:03 +0900

    Fix DirectoryNotFound

--- src/Models/DataTypes/UserSetting.cs
+++ src/Models/DataTypes/UserSetting.cs
@@ -10,17 +10,28 @@ namespace Yatsugi.Models.DataTypes
         private const string SETTINGS_DIRECTORY_NAME = "Yatsugi";
         private const string SETTINGS_FILE_NAME = "settings.dat";
 
-        public static string SettingsFilePath
+        public static string DataDirectory
             => Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData),
-                SETTINGS_DIRECTORY_NAME,
+                SETTINGS_DIRECTORY_NAME
+            );
+
+        public static string SettingsFilePath
+            => Path.Combine(
+                DataDirectory,
                 SETTINGS_FILE_NAME
             );
+
+        private UserSettings()
+        {
+
+        }
 
         public void WriteAll()
         {
             var serializer = new SerializerBuilder().Build();
             var yaml = serializer.Serialize(this);
+            Directory.CreateDirectory(Path.GetDirectoryName(SettingsFilePath));
             File.WriteAllText(SettingsFilePath, yaml);
         }
 
@@ -32,15 +43,21 @@ namespace Yatsugi.Models.DataTypes
                 var serializer = new DeserializerBuilder().Build();
                 return serializer.Deserialize<UserSettings>(yaml);
             }
-            else return null;
+            else
+            {
+                return Initalize();
+            }
         }
 
-        public void Reset()
+        public static UserSettings Initalize()
         {
             if (File.Exists(SettingsFilePath))
             {
                 File.Delete(SettingsFilePath);
             }
+            var defaultSetting = new UserSettings();
+            defaultSetting.WriteAll();
+            return defaultSetting;
         }
     }
 }
