aec0e25dae92faed0aa338d925b9e1c7f6e3b707
Akihisa Yagi <capra314cabra@gmail.com>
2020-04-02 13:10:38 +0900

    Add event types

--- src/ViewModels/StartMenuViewModel.cs
+++ src/ViewModels/StartMenuViewModel.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Avalonia.Input;
 using ReactiveUI;
 
 using Yatsugi.Models;
@@ -15,17 +17,40 @@ namespace Yatsugi.ViewModels
     {
         public StartMenuViewModel()
         {
-
-        }
-
-        private void OnReturnButtonClicked()
-        {
-            ToolDataBase.LoadAll();
+            OnMoveUserControl = ReactiveCommand.Create<StartMenuViewEvents, StartMenuViewEvents>(eventType => eventType);
         }
 
         private void OnLentButtonClicked()
         {
-            ToolDataBase.RecordAll();
+            OnMoveUserControl
+                .Execute(StartMenuViewEvents.ON_LENT_BUTTON_CLICKED)
+                .Subscribe();
         }
+
+        private void OnReturnButtonClicked()
+        {
+            OnMoveUserControl
+                .Execute(StartMenuViewEvents.ON_RETURN_BUTTON_CLICKED)
+                .Subscribe();
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F11)
+            {
+                OnMoveUserControl
+                    .Execute(StartMenuViewEvents.ON_SETTINGS_KEY_PUSHED)
+                    .Subscribe();
+            }
+        }
+
+        public ReactiveCommand<StartMenuViewEvents, StartMenuViewEvents> OnMoveUserControl { get; set; }
+    }
+
+    public enum StartMenuViewEvents
+    {
+        ON_LENT_BUTTON_CLICKED,
+        ON_RETURN_BUTTON_CLICKED,
+        ON_SETTINGS_KEY_PUSHED
     }
 }
