e06c5a02e6468b57df50c88b2ac3dfd627634c91
Akihisa Yagi <capra314cabra@gmail.com>
2020-04-03 10:34:31 +0900

    Add ViewModels

---
+++ src/ViewModels/ReturnViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+using Avalonia.Input;
+
+using Yatsugi.Models;
+
+namespace Yatsugi.ViewModels
+{
+    public class ReturnViewModel : ViewModelBase
+    {
+        public ReturnViewModel()
+        {
+
+        }
+    }
+}
