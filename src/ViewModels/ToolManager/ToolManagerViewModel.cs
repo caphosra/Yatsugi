2b489f0804e1b28fafbdd8114cdd0f092fd74d81
Akihisa Yagi <capra314cabra@gmail.com>
2020-06-02 14:08:58 +0900

    Implement QR code writer

--- src/ViewModels/ToolManager/ToolManagerViewModel.cs
+++ src/ViewModels/ToolManager/ToolManagerViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Reactive;
 using System.Reactive.Linq;
@@ -8,12 +9,14 @@ using System.Threading.Tasks;
 using System.Linq;
 
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Input;
 using MessageBox.Avalonia;
 using MessageBox.Avalonia.DTO;
 using MessageBox.Avalonia.Enums;
 using ReactiveUI;
+using QRCoder;
 
 using Yatsugi.Models;
 using Yatsugi.Models.DataTypes;
@@ -58,6 +61,9 @@ namespace Yatsugi.ViewModels
                 {
                     MoveToHistory(guid);
                 });
+            viewModel.OnQRCodeButtonClicked
+                .Take(1)
+                .Subscribe(OnGenerateQRCodeButtonClicked);
             viewModel.OnDeleteButtonClicked
                 .Take(1)
                 .Subscribe(async (guid) =>
@@ -118,6 +124,28 @@ namespace Yatsugi.ViewModels
                     MoveToToolList();
                 });
             Content = viewModel;
+        }
+
+        public async void OnGenerateQRCodeButtonClicked(Guid id)
+        {
+            var tool = ToolDataBase.Tools
+                .Single((tool) => tool.ID == id);
+
+            var dialog = new SaveFileDialog();
+            dialog.InitialFileName = $"{tool.Name}-QRCode.jpg";
+            dialog.Directory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            var path = await dialog.ShowAsync((App.Current as App).Window);
+
+            if (path != null)
+            {
+                var qrGenerator = new QRCodeGenerator();
+                var qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+                var qrCode = new QRCode(qrCodeData);
+                var qrCodeImage = qrCode.GetGraphic(20);
+                qrCodeImage.Save(path, ImageFormat.Jpeg);
+            }
+
+            MoveToToolList();
         }
     }
 }
