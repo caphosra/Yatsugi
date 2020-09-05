5f1450977ab552e7905b47ca7de7cc2079fb95ab
Akihisa Yagi <capra314cabra@gmail.com>
2020-09-05 22:31:49 +0900

    Reimplement DataManager

--- src/renderer/dataManager.ts
+++ src/renderer/dataManager.ts
@@ -37,35 +37,88 @@ export class DataManager<T extends YatsugiGroup | YatsugiTool> {
         }
     }
 
-    add(item: T) {
-        return new Promise<void>((resolve, reject) => {
-            ipcRenderer.send(`database-add-${this.kind}`, item);
-            ipcRenderer.on(`database-add-${this.kind}-reply`, (e, succeeded) => {
-                if (succeeded && this.data) {
-                    this.data = this.data.filter((val) => val.id != item.id);
-                    this.data.push(item);
-                    resolve();
-                }
-                else {
-                    reject();
-                }
-            });
-        });
+    async lentItem(groupID: string, toolIDs: string[]) {
+        if (!this.validLentItem(toolIDs)) {
+            throw "もう既に貸出されている器材を貸し出そうとしています。";
+        }
+        for (const id of toolIDs) {
+            const succeeded = await this.sendRequestAsync<boolean>("database-lent-tool", groupID, id);
+
+            if (!succeeded) {
+                this.gets();
+                throw "器材貸出に失敗しました。\nファイルへのアクセス権限等を再度確認して下さい。";
+            }
+        }
+        this.gets();
     }
 
-    delete(itemID: string) {
-        return new Promise<void>((resolve, reject) => {
-            ipcRenderer.send(`database-delete-${this.kind}`, itemID);
-            ipcRenderer.on(`database-delete-${this.kind}-reply`, (e, succeeded) => {
-                if (succeeded && this.data) {
-                    this.data = this.data.filter((val) => val.id != itemID);
-                    resolve();
+    async returnItem(toolIDs: string[]) {
+        if (!this.validReturnItem(toolIDs)) {
+            throw "もう既に返却されている器材を返却しようとしています。";
+        }
+        for (const id of toolIDs) {
+            const succeeded = await this.sendRequestAsync<boolean>("database-return-tool", id);
+
+            if (!succeeded) {
+                this.gets();
+                throw "器材返却に失敗しました。\nファイルへのアクセス権限等を再度確認して下さい。";
+            }
+        }
+        this.gets();
+    }
+
+    async add(item: T) {
+        const succeeded = await this.sendRequestAsync<boolean>(`database-add-${this.kind}`, item);
+
+        if (!succeeded) {
+            throw `コンテンツの追加に失敗しました。\n(Type:${this.kind} ID: ${item.id})`;
+        }
+        this.gets();
+    }
+
+    async delete(itemID: string) {
+        const succeeded = await this.sendRequestAsync<boolean>(`database-delete-${this.kind}`, itemID);
+
+        if (!succeeded) {
+            throw `コンテンツの削除に失敗しました。\n(Type:${this.kind} ID: ${itemID})`;
+        }
+        this.gets();
+    }
+
+    private validLentItem(toolIDs: string[]): boolean {
+        if (!this.data) {
+            this.gets();
+            return this.validLentItem(toolIDs);
+        }
+        else {
+            for (const item of this.data) {
+                if (toolIDs.includes(item.id)) {
+                    const tool = item as YatsugiTool;
+                    if (!tool.getGroup()) {
+                        return false;
+                    }
                 }
-                else {
-                    reject();
+            }
+            return true;
+        }
+    }
+
+    private validReturnItem(toolIDs: string[]): boolean {
+        if (!this.data) {
+            this.gets();
+            return this.validReturnItem(toolIDs);
+        }
+        else {
+            for (const item of this.data) {
+                if (toolIDs.includes(item.id)) {
+                    const tool = item as YatsugiTool;
+                    if (!tool.getGroup()) {
+                        return false;
+                    }
                 }
-            });
-        });
+            }
+            return true;
+        }
     }
 
     private convertFromJson(item: T) {
@@ -75,6 +128,15 @@ export class DataManager<T extends YatsugiGroup | YatsugiTool> {
         else {
             return new YatsugiTool(item as YatsugiTool) as T;
         }
+    }
+
+    private sendRequestAsync<Return>(channel: string, ...args: any[]) {
+        return new Promise<Return>((resolve, reject) => {
+            ipcRenderer.send(channel, args);
+            ipcRenderer.on(`${channel}-reply`, (e, ret) => {
+                resolve(ret);
+            });
+        });
     }
 }
 
