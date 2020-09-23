84f1fdf3953ecfcfacfb2939a1d1fe28b7fbf0ad
Akihisa Yagi <capra314cabra@gmail.com>
2020-09-23 20:48:23 +0900

    Implement TagEditor

--- src/renderer/components/tool-list/toolEditor.tsx
+++ src/renderer/components/tool-list/toolEditor.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Button, Card, InputGroup, FormControl } from "react-bootstrap";
+import { Button, Card, Form } from "react-bootstrap";
 
+import { ToolTag, toString } from "../../../lib/yatsugiTag";
 import { YatsugiTool } from "../../../lib/yatsugiTool";
 import { toolData } from "../../dataManager";
 
@@ -20,15 +21,18 @@ export class ToolEditor extends React.Component<IToolEditorProps, IToolEditorSta
         this.state = { };
 
         this.toolNameRef = React.createRef();
+        this.tagRef = React.createRef();
     }
 
     toolNameRef: React.RefObject<HTMLInputElement>;
+    tagRef: React.RefObject<HTMLSelectElement>;
 
     saveButtonClicked = async () => {
         this.saveButtonClicked = async () => { };
 
         let tool = this.props.editTool;
         tool.name = this.toolNameRef.current?.value ?? "no name";
+        tool.tag = Number.parseInt(this.tagRef.current?.value ?? "0");
         try {
             await toolData.add(tool);
             this.props.onFinished(true);
@@ -63,31 +67,38 @@ export class ToolEditor extends React.Component<IToolEditorProps, IToolEditorSta
                     器材一覧
                 </h1>
                 <Card style={bodyStyle} body>
-                    <InputGroup className="mb-3" contentEditable={false}>
-                        <InputGroup.Prepend>
-                            <InputGroup.Text>ID :</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl
-                            value={this.props.editTool.id}
-                        />
-                    </InputGroup>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Prepend>
-                            <InputGroup.Text>Name :</InputGroup.Text>
-                        </InputGroup.Prepend>
-                        <FormControl
-                            placeholder="器材名"
-                            ref={this.toolNameRef}
-                            defaultValue={this.props.editTool.name}
-                        />
-                    </InputGroup>
-                    <Button onClick={this.saveButtonClicked}>
-                        保存
-                    </Button>
-                    &nbsp;
-                    <Button onClick={this.cancelButtonClicked}>
-                        キャンセル
-                    </Button>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>ID</Form.Label>
+                            <Form.Control type="text" value={this.props.editTool.id} readOnly />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="text" ref={this.toolNameRef} defaultValue={this.props.editTool.name} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Tag</Form.Label>
+                            <Form.Control as="select" ref={this.tagRef}>
+                                {
+                                    Array.from(Array(6).keys()).map((_, val) => {
+                                        const tag: ToolTag = val;
+                                        return (
+                                            <option value={val}>{toString(tag)}</option>
+                                        )
+                                    })
+                                }
+                            </Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Button onClick={this.saveButtonClicked}>
+                                保存
+                            </Button>
+                            &nbsp;
+                            <Button onClick={this.cancelButtonClicked}>
+                                キャンセル
+                            </Button>
+                        </Form.Group>
+                    </Form>
                 </Card>
             </div>
         );
